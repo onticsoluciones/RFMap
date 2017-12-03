e2a20e3b098b8d86b222eed9d30b924c1b1668d2
alfonsomoratalla <alfonso.moratalla@gmail.com>
2017-12-03 02:51:46 +0100

    Added generate random probe from file

--- core/plugins/karma_detector/karma.sh
+++ core/plugins/karma_detector/karma.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Plugin Karma Detector
 
-/usr/sbin/mdk3 wlan0mon p -e prueba__karma1 &
+probe=`shuf -n 1 frutas.lst``shuf -n 1 frutas.lst` # Generate random probe from file
 
-/usr/sbin/tcpdump -l -i wlan0mon -e -s 256 type mgt subtype probe-resp or subtype probe-req 2>/dev/null| grep "prueba__karma1" | while read b; do
+/usr/sbin/mdk3 wlan0mon p -e $probe &
+
+/usr/sbin/tcpdump -l -i wlan0mon -e -s 256 type mgt subtype probe-resp or subtype probe-req 2>/dev/null| grep $probe | while read b; do
     datos=( $b )
     echo [{"entity_id":${datos[7]},"name":${datos[12]},"frequency":0,"bandwidth":40.00,"rssi":0,"extra":{"Attack":"Karma"}]
     break
