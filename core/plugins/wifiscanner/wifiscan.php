3faa41422d5b0bb9978e2e21c2f5d5482e23d1ed
Alejandro Sánchez <contacto@alsanchez.es>
2017-12-03 06:01:57 +0100

    Prevent duplicate bssid results

--- core/plugins/wifiscanner/wifiscan.php
+++ core/plugins/wifiscanner/wifiscan.php
@@ -6,14 +6,23 @@ exec('nmcli -f ssid,bssid,mode,freq,rate,signal,security dev wifi', $output);
 // DIRECT-r8C43x Series  86:25:19:07:B5:88  Infra  2412 MHz  54 Mbit/s  40      WPA2
 
 $data = [];
+$foundBssids = [];
 foreach($output as $line)
 {
     preg_match('/^(.+)\s+(\S{2}:\S{2}:\S{2}:\S{2}:\S{2}:\S{2})\s+(\S+)\s+(\d+) MHz\s+(\d+ Mbit\/s)\s+(\d+)\s+(\S+)$/', $line, $matches);
     if($matches)
     {
+        $bssid = trim($matches[2]);
+        
+        if(isset($foundBssids[$bssid]))
+        {
+            continue;
+        }
+        $foundBssids[$bssid] = 1;
+        
         $data[] = [
             'name' => trim($matches[1]),
-            'entity_id' => trim($matches[2]),
+            'entity_id' => $bssid,
             'frequency' => $matches[4],
             'bandwidth' => 20,
             'rssi' => ($matches[6] / 2) - 100,
