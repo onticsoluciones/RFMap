8049f8e944258cfd8886d4144991008bc6ec912e
alfonsomoratalla <alfonso.moratalla@gmail.com>
2017-12-03 03:07:23 +0100

    Check too if Karma is not active

--- core/plugins/karma_detector/karma.sh
+++ core/plugins/karma_detector/karma.sh
@@ -7,7 +7,7 @@ probe=`shuf -n 1 frutas.lst``shuf -n 1 frutas.lst` # Generate random probe from 
 
 /usr/sbin/tcpdump -l -i wlan0mon -e -s 256 type mgt subtype probe-resp or subtype probe-req 2>/dev/null| grep $probe | while read b; do
     datos=( $b )
-    echo [{"entity_id":${datos[7]},"name":${datos[12]},"frequency":0,"bandwidth":40.00,"rssi":0,"extra":{"Attack":"Karma"}]
+    echo [{"entity_id":${datos[7]},"name":${datos[12]},"frequency":0,"bandwidth":20.00,"rssi":0,"extra":{"Karma":"1"}]
     break
 done
 killall -9 mdk3
